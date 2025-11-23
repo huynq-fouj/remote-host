87fa40c372eba91345ee7a5805d07b75d12ebebb
Nguyễn Quang Huy <huynq5@softdreams.vn>
2025-11-24 03:38:30 +0700

    fisrt commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
+import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      loadRemoteModule({
+        remoteName: 'products',
+        exposedModule: './Component',
+      }).then((m) => {
+        return m.App; 
+      }),
+  },
+  {
+    path: 'cart',
+    loadComponent: () =>
+      loadRemoteModule({
+        remoteName: 'cart',
+        exposedModule: './Component',
+      }).then((m) => {
+        return m.App; 
+      }),
+  },
+];
