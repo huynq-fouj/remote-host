366921e444797f3680046f98171b4361ba347d52
Nguyễn Quang Huy <nguyenqunaghuylt2002@gmail.com>
2025-11-24 14:36:47 +0700

    update environment

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    manifest: '/assets/federation.manifest.development.json'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    manifest: '/assets/federation.manifest.json'
+};
